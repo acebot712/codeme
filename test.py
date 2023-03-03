df7bfe5f001c3601f2e2e8db0c1200dd9a5807c7
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-03-04 01:02:46 +0530

    workflow triggering test

---
+++ test.py
@@ -0,0 +1,34 @@
+# setup CI pipeline
+
+1. Install the necessary tools:
+
+- Python
+- Git
+- Continuous Integration (CI) tools such as Jenkins, Travis CI, or CircleCI
+
+2. Set up your project repository:
+
+- Create a new repository on GitHub or other version control system
+- Add your project files to the repository
+
+3. Configure your CI tool:
+
+- Create a new project in your CI tool
+- Connect the project to your repository
+- Configure the build steps for your project
+
+4. Create a Python script to run your tests:
+
+- Create a Python script that will run your tests
+- Add the script to your repository
+
+5. Set up your CI pipeline:
+
+- Configure your CI tool to run the Python script when changes are made to the repository
+- Set up notifications to alert you when the tests pass or fail
+
+6. Monitor your pipeline:
+
+- Monitor the status of your pipeline to ensure that your tests are running correctly
+- Make adjustments to the pipeline as needed to ensure that your tests are running correctly
+
