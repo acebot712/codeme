d9bd8c762a9ae6819f0d691574d54b3b84080fe5
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-03-07 10:48:33 +0530

    forked dataset

--- finetune.py
+++ finetune.py
@@ -50,3 +50,60 @@ input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
 output = model.generate(input_ids=input_ids, max_length=1024, num_return_sequences=1)
 generated_code = tokenizer.decode(output[0], skip_special_tokens=True)
 print(generated_code)
+
+# fine tune a gptneo model from hugging face with pytorch using prompt and text pairs
+
+#import necessary libraries
+import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+#instantiate tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+#define prompt and text pairs
+prompt = "The cat sat on the"
+text_pairs = [
+    "mat. It was very comfortable.",
+    "chair. It didn't like it very much."
+]
+
+#encode prompt and text pairs
+encoded_prompt = tokenizer.encode(prompt, add_special_tokens=False)
+encoded_text_pairs = [tokenizer.encode(pair, add_special_tokens=False) for pair in text_pairs]
+
+#create input tensors
+input_ids = torch.tensor([encoded_prompt + pair for pair in encoded_text_pairs])
+
+#fine tune the model
+model.train()
+model.fit(input_ids, labels=input_ids)
+
+# calculate  Normalized Discounted Cumulative Gain from generated code token list and ground truth code tokens list
+
+#importing libraries
+import numpy as np
+
+#ground truth code tokens list
+ground_truth_code_tokens = ['if', 'else', 'for', 'while', 'break', 'continue']
+
+#generated code token list
+generated_code_tokens = ['if', 'else', 'for', 'while', 'break', 'continue', 'int', 'float']
+
+#calculating normalized discounted cumulative gain
+def ndcg(ground_truth_code_tokens, generated_code_tokens):
+    #calculating ideal discounted cumulative gain
+    idcg = 0
+    for i in range(len(ground_truth_code_tokens)):
+        idcg += (2**i - 1)/np.log2(i+2)
+    
+    #calculating discounted cumulative gain
+    dcg = 0
+    for i in range(len(generated_code_tokens)):
+        if generated_code_tokens[i] in ground_truth_code_tokens:
+            dcg += (2**i - 1)/np.log2(i+2)
+    
+    #calculating normalized discounted cumulative gain
+    ndcg = dcg/idcg
+    
+    return ndcg
